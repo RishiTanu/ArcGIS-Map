9ea2d08c95e3088fd7b81b01a912d62ddac9b9b9
unknown <singhrishikesh500@gmail.com>
2024-02-06 14:35:59 +0530

    add marker

--- app/src/main/java/com/example/aarcgis/MainActivity.kt
+++ app/src/main/java/com/example/aarcgis/MainActivity.kt
@@ -4,20 +4,32 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.esri.arcgisruntime.geoanalysis.LocationViewshed
 import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.geometry.SpatialReferences
 import com.esri.arcgisruntime.layers.ArcGISSceneLayer
 import com.esri.arcgisruntime.mapping.ArcGISScene
 import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource
 import com.esri.arcgisruntime.mapping.Basemap
 import com.esri.arcgisruntime.mapping.view.AnalysisOverlay
+import com.esri.arcgisruntime.mapping.view.AtmosphereEffect
 import com.esri.arcgisruntime.mapping.view.Camera
+import com.esri.arcgisruntime.mapping.view.Graphic
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay
+import com.esri.arcgisruntime.mapping.view.LightingMode
 import com.esri.arcgisruntime.mapping.view.SceneView
-
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol.Style
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var sceneView: SceneView
-
-
+    // Define this as a global property if you need to cancel it when the activity is destroyed
+    private var animationJob: Job? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,25 +46,69 @@ class MainActivity : AppCompatActivity() {
             basemap = Basemap.createImagery()
 
             // Add elevation data
-            baseSurface.elevationSources.add(ArcGISTiledElevationSource("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"))
+            baseSurface.elevationSources.add(
+                ArcGISTiledElevationSource(
+                    "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"
+                )
+            )
 
             // Add building data
-            operationalLayers.add(ArcGISSceneLayer("https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Building_Berlin/SceneServer"))
+            operationalLayers.add(
+                ArcGISSceneLayer(
+                    "https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Building_Berlin/SceneServer"
+                )
+            )
+
+            // Apply sun lighting with shadows to the base surface
+            sceneView.sunLighting = LightingMode.LIGHT_AND_SHADOWS
+
+            // Apply realistic atmosphere effect to the scene view
+            sceneView.atmosphereEffect = AtmosphereEffect.REALISTIC
+
+            // Create a camera and set it as the viewpoint for when the scene loads
+            val initialCameraPosition = Point(13.377704, 52.516275, 1450.0, SpatialReferences.getWgs84())
+            val camera = Camera(initialCameraPosition, 300.0, 75.0, 0.0)
+            sceneView.setViewpointCamera(camera)
+
+            // Add marker after the scene is set up
+            addMarkerAtCameraPosition(initialCameraPosition)
         }
 
-        // Create a camera and set it as the viewpoint for when the scene loads
+        /*// Create a camera and set it as the viewpoint for when the scene loads
         val camera = Camera(Point(13.377704, 52.516275, 450.0), 300.0, 75.0, 0.0)
+
         sceneView.setViewpointCamera(camera)
+        addMarkerAtCameraPosition(initialCameraPosition)*/
+        addViewshedAnalysis()
     }
+    private fun addMarkerAtCameraPosition(initialPosition: Point) {
+        // Adjust the altitude to 1000
+        val startPosition = Point(initialPosition.x, initialPosition.y, 10000.0, initialPosition.spatialReference)
 
-    private fun addGraphicsOverlay() {
-        // Adding a graphics overlay for dynamic content
-        val graphicsOverlay = GraphicsOverlay().apply {
-           // renderingMode = RenderingMode.DYNAMIC
-        }
+        // Create a simple marker symbol
+        val markerSymbol = SimpleMarkerSymbol(Style.CIRCLE, 0xFF0000FF.toInt(), 20f) // Blue circle
+
+        // Create a graphic with the point and symbol
+        val markerGraphic = Graphic(startPosition, markerSymbol)
+
+        // Create a graphics overlay and add the graphic to it
+        val graphicsOverlay = GraphicsOverlay()
+        graphicsOverlay.graphics.add(markerGraphic)
+
+        // Add the graphics overlay to the scene view
         sceneView.graphicsOverlays.add(graphicsOverlay)
-    }
 
+        // Animate the marker from left to right
+        val moveDistance = 0.0001 // Adjust this value for the distance of each step
+        animationJob = CoroutineScope(Dispatchers.Main).launch {
+            while (isActive) {
+                val currentPoint = markerGraphic.geometry as Point
+                val newPoint = Point(currentPoint.x + moveDistance, currentPoint.y, currentPoint.z, currentPoint.spatialReference)
+                markerGraphic.geometry = newPoint
+                delay(1000) // Adjust this value for the interval of movement
+            }
+        }
+    }
     private fun addViewshedAnalysis() {
         // Adding an analysis like Viewshed
         val observerPoint = Point(13.377704, 52.516275, 450.0) // Long, Lat, Altitude
@@ -70,24 +126,27 @@ class MainActivity : AppCompatActivity() {
         sceneView.analysisOverlays.add(analysisOverlay)
     }
 
+
+
+
+
+
     private fun controlCamera() {
         // Controlling the camera
         val cameraPosition = Camera(Point(13.377704, 52.516275, 450.0), 300.0, 75.0, 0.0)
         sceneView.setViewpointCameraAsync(cameraPosition, 5f) // 5 seconds animation
     }
-
     override fun onPause() {
         sceneView.pause()
         super.onPause()
     }
-
     override fun onResume() {
         super.onResume()
         sceneView.resume()
     }
-
     override fun onDestroy() {
         sceneView.dispose()
+        animationJob?.cancel() // Cancel the animation job when the activity is destroyed
         super.onDestroy()
     }
 }
